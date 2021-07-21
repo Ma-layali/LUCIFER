089d35be421a4f3e7fc37ebde109ba8c1fc427a0
Ma-layali <83540012+Ma-layali@users.noreply.github.com>
2021-07-21 10:24:51 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -13,45 +13,42 @@ Click /about for BIOGRAPHICAL INFORMATION
 """    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b>Lucifer Morningstar</b> , also known as <b>Samael or The Lightbringer</b>, is one of <i>God's angels</i> and the infamous <i>ruler of Hell</i>. He served as the <i>King of Hell</i> for eons until he decided that he needed a change of scenery. He is commonly known as the <b>Devil</b> or <b>Satan by humans</b>. Having grown tired of ruling the Underworld, after being cast out and fallen from <i>Heaven</i>, Lucifer voluntarily left his position in Hell to become a nightclub owner in Los Angeles, operating an establishment called Lux with his closest friend and senior lieutenant, the demon <b>Mazikeen</b>.
 
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
+Lucifer started working alongside LAPD homicide detective Chloe Decker after he witnessed the murder of a close acquaintance of his. This position gave him an outlet to punish sinners.
 
-<b>Bot Commands (Works Only In Groups) :</b>
+With his wings returned and his Devil face gone, Lucifer believed that a master criminal known as The Sinnerman was behind it all and set out to punish him. He also started a friendship with Charlotte Richards after her experience in Hell left her broken. After a long chase and misdirection, Lucifer learned that the LAPD's new lieutenant was not only the Sinnerman, but the world's first murderer, Cain, but he had nothing to do with his latest conundrum. This led Lucifer to think that God did it all to foil Cain's plans, so Lucifer set out to help get rid of his mark so he could finally die. He soon grew jealous when Cain and Chloe started dating, and later became engaged, so decided to tell her how he felt, but he backed out when the latter called it off. After the death of Charlotte and Amenadiel getting his wings back, Lucifer realized that all the changes to his body were his doing, believing he deserved what he got. Once Lucifer finally killed Cain, his Devil face returned, just as Chloe witnessed it.
 
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+Lucifer's relationship with Chloe took a turn after she initially betrayed him to send him back to Hell with the help of Father William Kinley. Though Chloe realized she was wrong, Lucifer couldn't bring himself to forgive her, and things only got worse when Eve showed up and the two resumed their former relationship. Lucifer and Chloe eventually reconciled and he returned to consulting for the LAPD. Unfortunately, because of Eve's influence, Lucifer started inflicting serious punishment on the guilty, before he realized what he was becoming and broke up with her to prevent a world ending prophecy. However, the prophecy came true when Demons started invading LA, forcing Lucifer to return to Hell to keep them in line. Before leaving, Lucifer and Chloe admitted their feelings for each other and he resumed his rule over Hell.
 
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
+After spending thousands of years later (in Hell time), Lucifer was forced to return when his twin brother Michael showed up on Earth and started wreaking havoc. His relationship with Chloe took another turn after she learned that she was a gift from God, and his own confidence was shattered when he discovered Michael had been manipulating him since the beginning. Lucifer and Chloe, however, were able to see past their issues and finally became a couple, but Lucifer's mojo passed on to Chloe, and he became invulnerable again, even whilst in the presence of his girlfriend. After Chloe was kidnapped by his twin and subsequently rescued, Lucifer was forced to finally confront his true feelings for her but was halted when Michael and Maze attacked him and Amenadiel. Their sibling squabble was broken up, however, when God showed up. God eventually revealed that he was going to retire, leading to a conflict between Lucifer and Michael over who would take his place, which ended with Michael apparently victorious. However, after Michael killed Chloe, Lucifer sacrificed himself to return to Heaven and resurrect her. Lucifer's selfless action resulted in his own resurrection as the new God.
+♾ @MzoneChat ♾
 
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/im_godgroot">Aashaan</a></i></b>
+    ABOUT_TEXT = """ <center><b><i><u>BIOGRAPHICAL INFORMATION</u></i></b><center>
+<b>SPECIES:</b> <code>Archangel</code>
+            <code>God</code>
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>STATUS :</b> <code>Extant</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>DATE OF BIRTH :</b> <code>Before the creation of the Universe</code>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/Fgroots/hulkbotv2">Click Me</a></i>
+<b>AGE :</b> <code>3.8 Billion Years Old</code>
+
+<b>GENDER :</b>  <code>Male</code>
+<u><center>AFFILIATIONS</center></u>
+<b>HOME</b> : <code>Silver City</code>
+               <code>Heaven</code> (formerly)
+               <code>Hell</code> (formerly)
+               <code>Apartment, Los Angeles</code> (formerly)
+               <code>Lux, Los Angeles, Earth-666</code> (Currently)
+<b>RELATIVES</b> : <code>God</code> (father)
+                        <code>Goddess</code> (mother)
+                        <code>Angels</code> (siblings)
+                        <code>Charlie</code> (nephew)
+                        <code>Chloe Decker</code> (girlfriend)
+                        <code>Eve</code> (ex-girlfriend)
+                        <code>Mazikeen</code> (former lieutenant; friend)
+<b>Master/Creater : @Mzoneceo</b>
 """
