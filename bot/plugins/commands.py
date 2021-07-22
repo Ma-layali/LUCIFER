de2b1633e19f84520e66faa09c1724e10181a1d8
Ma-layali <83540012+Ma-layali@users.noreply.github.com>
2021-07-22 14:45:05 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption ="<code>" + file_name + "</code>",
+                caption =caption,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
